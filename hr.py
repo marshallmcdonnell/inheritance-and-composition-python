7b6f056f2f77fdc0ab51fe59668bd7d0b637a460
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-08 16:09:37 -0500

    Add runtime policy change via composition

--- hr.py
+++ hr.py
@@ -67,6 +67,27 @@ class CommissionPolicy(SalaryPolicy):
         return fixed + self.commission
 
 
+class LTDPolicy:
+    def __init__(self):
+        self._base_policy = None
+
+    def track_work(self, hours):
+        self._check_base_policy()
+        return self._base_policy.track_work(hours)
+
+    def calculate_payroll(self):
+        self._check_base_policy()
+        base_salary = self._base_policy.calculate_payroll()
+        return base_salary * 0.6
+
+    def apply_to_policy(self, base_policy):
+        self._base_policy = base_policy
+
+    def _check_base_policy(self):
+        if not self._base_policy:
+            raise RuntimeError('Base policy missing')
+
+
 _payroll_system = _PayrollSystem()
 
 
--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 import json
 
-from hr import calculate_payroll
+from hr import calculate_payroll, LTDPolicy
 from productivity import track
 from employees import employee_database, Employee
 
@@ -8,6 +8,10 @@ def print_dict(d):
     print(json.dumps(d, indent=2))
 
 employees = employee_database.employees
+
+sales_employee = employees[2]
+ltd_policy = LTDPolicy()
+sales_employee.apply_payroll_policy(ltd_policy)
 
 track(employees, 40)
 calculate_payroll(employees)
