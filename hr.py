33bcedf85ae0dd8da259ffac0fa21207e348c45a
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-08 13:52:39 -0500

    Multiple inheritance w/o diamond problem

--- hr.py
+++ hr.py
@@ -1,5 +1,3 @@
-from abc import ABC, abstractmethod
-
 class PayrollSystem:
     def calculate_payroll(self, employees):
         print('Calculate Payroll')
@@ -8,3 +6,30 @@ class PayrollSystem:
             print(f'Payroll for: {employee.id} - {employee.name}')
             print(f'- Check amount: {employee.calculate_payroll()}')
             print('')
+
+class SalaryPolicy:
+    def __init__(self,  weekly_salary):
+        super().__init__()
+        self.weekly_salary = weekly_salary
+
+    def calculate_payroll(self):
+        return self.weekly_salary
+
+
+class HourlyPolicy:
+    def __init__(self, hours_worked, hour_rate):
+        self.hours_worked = hours_worked
+        self.hour_rate = hour_rate
+
+    def calculate_payroll(self):
+        return self.hours_worked * self.hour_rate
+
+
+class CommissionPolicy(SalaryPolicy):
+    def __init__(self, weekly_salary, commission):
+        super().__init__(weekly_salary)
+        self.commission = commission
+
+    def calculate_payroll(self):
+        fixed = super().calculate_payroll()
+        return fixed + self.commission
--- productivity.py
+++ productivity.py
@@ -3,5 +3,22 @@ class ProductivitySystem:
         print('Track Employee Productivity')
         print('===========================')
         for employee in employees:
-            employee.work(hours)
+            result = employee.work(hours)
+            print(f'{employee.name}: {result}')
         print('')
+
+class ManagerRole:
+    def work(self, hours):
+        return f'{self.name} screams and yells for {hours} hours.'
+
+class SecretaryRole:
+    def work(self, hours):
+        return f'{self.name} expends {hours} hours doing official paperwork.'
+
+class SalesRole:
+    def work(self, hours):
+        return f'{self.name} expends {hours} hours on the phone.'
+
+class FactoryRole:
+    def work(self, hours):
+        return f'{self.name} manufactures gadgets for {hours} hours.'
