7462d37dee90fc190d2b18c5f7e39239bf43b139
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-08 15:20:07 -0500

    Mixin via AsDictionaryMixin

--- program.py
+++ program.py
@@ -8,3 +8,13 @@ employee_database = EmployeeDatabase()
 employees = employee_database.employees
 productivity_system.track(employees, 40)
 payroll_system.calculate_payroll(employees)
+
+
+print('Using the AsDictionaryMixin')
+print('===========================')
+import json
+def print_dict(d):
+    print(json.dumps(d, indent=2))
+
+for employee in EmployeeDatabase().employees:
+    print_dict(employee.to_dict())
